8fed761f0915b172d6b4e1ac5be754b812c35c7a
Fatimaezzahralah <128908115+Fatimaezzahralah@users.noreply.github.com>
2024-07-26 00:50:27 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,23 @@
 import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js";
-import MoroccookiesRouter from "./routes/MoroccookiesRoute.js";
+import cors from"cors"
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
 
-
-
-// configuration of the app
-const app = express();
+// App config
+const app = express()
 const port = 4000
 
-// middleware
-app.use(express.json());
+
+// Middleware
+app.use(express.json())
 app.use(cors())
 
-// Connection of DB
+// DB Connection
 connectDB();
 
-// api endpoints
-app.use("/api/Moroccookies",MoroccookiesRouter)
-app.use("/images",express.static('uploads'))
+// API endpoints
+app.use("/api/user",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
